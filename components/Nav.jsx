a7f6b2f6fb52ecce873e0c2f6e90f49eec322f47
nimrod aviam <nimrod.a6@gmail.com>
2023-10-25 23:22:15 +0300

    sign in

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -4,9 +4,20 @@ import Image from 'next/image'
 import {useState, useEffect} from 'react'
 import {signIn, signOut, useSession, getProviders} from 'next-auth/react'
 
-
+ 
 const Nav = () => {
+    
     const isUserLoggedIn = true
+    const [providers, setProviders] = useState(null)
+    useEffect(()=>{
+        const setProviders = async ()=> {
+            const response = await getProviders()
+            setProviders(response)
+        }
+        setProviders()
+    },[])
+    
+
   return (
         <nav className="flex-between w-full mb-16 pt-3">
             <Link href="/" className="flex gap-2 flex-center"> 
@@ -41,7 +52,22 @@ const Nav = () => {
                         </Link>
                     </div>
                 ) : (
-                    <div>44</div>
+                    <>
+                    {providers && 
+                    Object.values(providers).map((provider)=>
+                    (
+                        <button 
+                        type="button"
+                        key={provider.name}
+                        onClick ={()=>signIn(provider.id)}
+                        className="black_btn"
+                        >
+                        Sign In
+                        </button>
+                    )
+                    )
+                    }
+                    </>
                 )
             }
             </div> 
