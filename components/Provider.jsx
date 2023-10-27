201c6afdc31fe3aa11aa0dd45ba2a44427356d1d
nimrod aviam <nimrod.a6@gmail.com>
2023-10-27 23:57:14 +0300

    set NextAuth

--- components/Provider.jsx
+++ components/Provider.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+"use client"
 
-const Provider = () => {
+import {SessionProvider} from 'next-auth/react'
+
+const Provider = ({children, session}) => {
   return (
-    <div>Provider</div>
+<SessionProvider session = {session}>
+    {children}
+</SessionProvider>
   )
 }
 
 export default Provider
+
+// 1:16  /
