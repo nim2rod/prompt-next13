6bc4490048c16ab54ac574106b4ad1f0d4b80691
nimrod aviam <nimrod.a6@gmail.com>
2023-11-01 20:19:00 +0200

    styling btn in prompt-card

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -65,7 +65,8 @@ handleEdit, handleDelete}) => {
 
         {session?.user.id === post.creator._id &&
         pathName === '/profile' && (
-          <div>
+          <div className="mt-5 flex-center gap-4 
+          border-t border-gray-100 pt-3">
             <p
             className="font-inter text-sm
             green_gradient cursor-pointer"
