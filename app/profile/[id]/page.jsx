fb3115207d9a1094a9afba5f0fbfd0d3984eeaf3
nimrod aviam <nimrod.a6@gmail.com>
2023-11-05 12:15:34 +0200

    user profile page

---
+++ app/profile/[id]/page.jsx
@@ -0,0 +1,33 @@
+'use client' 
+
+import {useState, useEffect} from 'react'
+import {useSearchParams} from "next/navigation"
+
+import Profile from '@components/Profile'  
+
+const UserProfile = ({params}) => {
+    const searchParams = useSearchParams()
+    const userName = searchParams.get("name")
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            const response = await fetch(`/api/users/${params?.id}/posts`)
+            const data = await response.json()
+
+            setPosts(data)
+        }
+
+      if(params?.id) fetchPosts()
+    }, [params.id])  
+
+    return (
+    <Profile
+    name="My"
+    desc= {`Welcome to ${userName} personalized Profile page`}
+    data={posts}
+    />
+  )
+}
+
+export default UserProfile
